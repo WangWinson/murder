3637dc8a25c0ce9599cacb4f46c4360b43b8b682
rolandpoulter <rolandpoulter@gmail.com>
2015-04-22 21:36:19 -0700

    crdt versioning, and delete work

--- example/client.js
+++ example/client.js
@@ -31,9 +31,8 @@ murder.sync().then(function () {
     var count = 0;
     murder.toArray().forEach(function (fellow) {
       if (count > 5) { return; }
-      if (fellow !== crow) {
-        setTimeout(fellow.fly.bind(fellow), 100 * count);
-      }
+      if (fellow !== crow)
+        { setTimeout(fellow.fly.bind(fellow), 100 * count); }
       count += 1;
     });
     setTimeout(flock,  Math.max(20000, Math.random() * 60000));
