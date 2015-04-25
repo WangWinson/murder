0d55bf221439f806811118c0b160646fd45d2d50
rolandpoulter <rolandpoulter@gmail.com>
2015-04-24 20:11:39 -0700

    some fixes

--- example/public/ui/Crow.jsx
+++ example/public/ui/Crow.jsx
@@ -8,6 +8,8 @@ window.Crow = React.createClass({
   mixins: [CRDTReactMixin],
 
   render: function() {
+    // if (this.state.x === 0 && this.state.y === 0)
+    //   { return ''; }
     var style = {
       left: (this.state.x * 100) + '%',
       top: (this.state.y * 100) + '%'
